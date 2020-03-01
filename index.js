8ba1bf9b128020a5762580a791b2173558e7ba8e
LiamCSDev <57903273+LiamCSDev@users.noreply.github.com>
2020-03-01 11:37:39 +0000

    Database Connection (Attempt)
    Updated database connection details,
    Attempted to connect using the example provided by expressjs, currently shows a connection but does not run the query.

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-var mysql = require('mysql')
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'M.Singh55@wlv.ac.uk',
-    password: 'Friday1995',
-    database: 'db.1933527'
-})
+// Install express server
+const express = require('express');
+const path = require('path');
 
-connection.connect()
+// Initialize express app
+const app = express();
+const router = express.Router();
 
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields){
-    if (err) throw err
-    console.log('The solution is: ', rows[0].solution)
-})
+// Serve static pages
+app.use(express.static('./'));
 
-connection.end();
+// Add API Routes 
+app.use('/api', router);
+
+// Specify public page entry point
+app.get('/', function(req, res) {
+    res.sendFile(path.join('/index.html'))
+});
+
+// Serve backend routes (async)
+app.get('/api', async function(req, res) {
+  res.status(200).send('Hello World API')
+  const result = mySqlTest();
+});
+
+// Specify port
+const port = process.env.PORT || 5001;
+
+// Start the app
+app.listen(port, () => {
+  console.log('App started on port: ' + port);
+});
+
+
+function mySqlTest() {
+    const mysql = require('mysql')
+    const connection =  mysql.createConnection({
+        host: 'mi-linux.wlv.ac.uk',
+        user: '1933527',
+        password: '707y1s',
+        database: 'db1933527',
+        debug : true,
+    });
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        if (error) throw console.log(error);
+        console.log('The solution is: ', results[0].solution);
+    });
+
+    connection.end();
+}
