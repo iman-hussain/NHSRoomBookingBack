a7ad3b594b39882f83db217453645aee5df602c9
1923122 <j.c.minayamorillo@wlv.ac.uk>
2020-03-02 22:38:35 +0000

    Delete Connection file, used connection as Express Middleware

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 // Install express server
 const express = require('express');
 const path = require('path');
-const oracledb = require('./connection');
+const oracledb = require('oracledb');
 
 // Initialize express app
 const app = express();
 const router = express.Router();
 
-// Serve static pages
-app.use('./', router);
-
-// Add API Routes 
-app.use('/api', router);
-app.use('/mysql', router);
+app.use((req, res, next) => {
+  oracledb.getConnection(
+  {
+    user          : "OPS$1415065",
+    password      : "P3nn1602",
+    connectString : "ora-srv.wlv.ac.uk:1521/catdb.wlv.ac.uk"
+  },
+  (error, conn) => {
+    if (error) {console.log(err);}
+    else{
+      req._oracledb = conn;
+      next();
+    }
+})});
 
 // Specify public page entry point
-app.get('/mysql', function(req, res) {
-    
+app.get('/rooms', async function(req, res) {
+  req._oracledb.execute("SELECT * FROM SHIP_TB", function(err, rows){
+    req._oracledb.close();
+    if (!err)
+    {
+      res.send(rows)
+    }
+    else
+    {
+      console.log('Error while performing Query.');
+    }
+  })
 });
 
 // Serve backend routes (async)
